7e8ec64cdec62dd0670aedd4d30edbddba3da240
Juan Ignacio Marchetto <43477348+JuanIgnacioMarchetto@users.noreply.github.com>
2023-06-13 17:52:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const itemContainer = document.getElementById("item-container");
 const itemInput = document.getElementById("item-input");
-
 const items = [];
-
 // Ejemplo de elementos de muestra
 items.push({ name: "Manzanas", quantity: 5 });
 items.push({ name: "Leche", quantity: 1 });
-
 function addItem() {
     const itemName = itemInput.value.trim();
     if (itemName !== "") {
@@ -15,47 +12,38 @@ function addItem() {
         renderItems();
     }
 }
-
 function updateQuantity(index, quantity) {
     items[index].quantity = parseInt(quantity);
     renderItems();
 }
-
 function deleteItem(index) {
     items.splice(index, 1);
     renderItems();
 }
-
 function renderItems() {
     itemContainer.innerHTML = "";
-
     items.forEach((item, index) => {
         const itemDiv = document.createElement("div");
         itemDiv.classList.add("item");
-
         const itemName = document.createElement("div");
         itemName.textContent = item.name;
-
         const itemQuantity = document.createElement("input");
         itemQuantity.type = "number";
         itemQuantity.value = item.quantity;
         itemQuantity.addEventListener("change", (event) => {
             updateQuantity(index, event.target.value);
         });
-
         const itemStatus = document.createElement("h2");
         if (item.quantity <= 2) {
             itemStatus.textContent = "Comprar";
         } else {
             itemStatus.textContent = "Suficiente";
         }
-
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Eliminar";
         deleteButton.addEventListener("click", () => {
             deleteItem(index);
         });
-
         itemDiv.appendChild(itemName);
         itemDiv.appendChild(itemQuantity);
         itemDiv.appendChild(itemStatus);
@@ -109,73 +97,6 @@ function loadItems() {
         items = JSON.parse(savedList);
         renderItems();
     }
-}
-
-function renderItems() {
-    itemContainer.innerHTML = "";
-
-    items.forEach((item, index) => {
-        const itemDiv = document.createElement("div");
-        itemDiv.classList.add("item");
-
-        const itemName = document.createElement("div");
-        itemName.textContent = item.name;
-
-        const itemQuantity = document.createElement("input");
-        itemQuantity.type = "number";
-        itemQuantity.value = item.quantity;
-        itemQuantity.addEventListener("change", (event) => {
-            updateQuantity(index, event.target.value);
-        });
-
-        const itemStatus = document.createElement("h2");
-        if (item.quantity <= 2) {
-            itemStatus.textContent = "Comprar";
-        } else {
-            itemStatus.textContent = "Suficiente";
-        }
-
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Eliminar";
-        deleteButton.addEventListener("click", () => {
-            deleteItem(index);
-        });
-
-        itemDiv.appendChild(itemName);
-        itemDiv.appendChild(itemQuantity);
-        itemDiv.appendChild(itemStatus);
-        itemDiv.appendChild(deleteButton);
-        itemContainer.appendChild(itemDiv);
-    });
-}
-
-renderItems();
-const itemContainer = document.getElementById("item-container");
-const itemInput = document.getElementById("item-input");
-
-const items = [];
-
-// Ejemplo de elementos de muestra
-items.push({ name: "Manzanas", quantity: 5 });
-items.push({ name: "Leche", quantity: 1 });
-
-function addItem() {
-    const itemName = itemInput.value.trim();
-    if (itemName !== "") {
-        items.push({ name: itemName, quantity: 0 });
-        itemInput.value = "";
-        renderItems();
-    }
-}
-
-function updateQuantity(index, quantity) {
-    items[index].quantity = parseInt(quantity);
-    renderItems();
-}
-
-function deleteItem(index) {
-    items.splice(index, 1);
-    renderItems();
 }
 
 function renderItems() {
